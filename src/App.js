ce815b5b54e2b5a78df055c901c033d41bedc39f
jamilahmed2 <ja0935991@gmail.com>
2023-03-02 23:48:45 +0500

    checkbox with useState hook

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ function App() {
   // useState returns a pair inside of an array
   // the first item in the array is the state variable
   // second item is a function that we use to change the state
+  // const [
+  //   status,
+  //   setStatus
+  // ] = useState('Not Delivered')
   const [
-    status,
-    setStatus
-  ] = useState('Not Delivered')
+    checked,
+    setChecked
+  ] = useState(false)
 
   return (
     <div className='app'>
-      <h1>The package is: {status}</h1>
-      <button onClick={() => setStatus("Delivered")}>Deliver</button>
-      <button onClick={() => setStatus('cancelled')}>cancel</button>
+      <input type='checkbox' value={checked} onChange={() => setChecked(!checked)}/>
+      <p>{checked ? 'Checked' : 'Not Checked'}</p>
     </div>
   );
 }
